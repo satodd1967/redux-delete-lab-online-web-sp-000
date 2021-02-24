b20a4e2672654e8438ad96ee82a35e218d739803
satodd1967 <seth.todd@outlook.com>
2021-02-23 21:09:46 -0600

    commit tree 80d6eaeb1f19c31b86ffa98204c656a6f09b1ebd

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,10 +1,25 @@
+import uuid from 'uuid';
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
 
-      return { ...state, bands: [...state.bands, action.name] }
+      const band = {
+        id: uuid(),
+        name: action.name
+      }
+
+      return { bands: state.bands.concat(band) };
+
+      // return { ...state, bands: [...state.bands, action.name] }
+
+    case 'DELETE_BAND':
+
+      let newState = {bands: state.bands.filter(band => band.id !== action.id)}
+
+      return newState
 
     default:
       return state;
